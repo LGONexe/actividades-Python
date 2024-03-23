2cbe5c666baef505b587b92829ef0cbeabdfb275
LG0N <151055608+LG0N@users.noreply.github.com>
2024-03-23 11:32:05 -0500

    primer comentario

---
+++ Selectivas/09-03-24ejercicio3.py
@@ -0,0 +1,12 @@
+while True:
+    compra=float (input("digite el valor de la compra"))
+    
+    balota=(input("elige el color de la balota)(rojo,amarillo,negro,azul"))
+
+if compra>=50000:
+    if balota=="rojo" :
+        descuento=compra*0.10
+    elif compra>=50000:
+        
+    elif balota=="azul":
+        descuento=compra
